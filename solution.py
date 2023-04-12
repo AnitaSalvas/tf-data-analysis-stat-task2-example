4366284d2b63049c6dc0212963b4b523aa8c107a
AnitaSalvas <130136433+AnitaSalvas@users.noreply.github.com>
2023-04-12 09:25:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,18 +4,16 @@ import numpy as np
 from scipy.stats import norm
 from scipy.stats import chi2
 
+
 chat_id = 5072617748 # Ваш chat ID, не меняйте название переменной
- 
+
 def solution(p: float, x: np.array) -> tuple:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
     alpha = 1 - p
     n = len(x)
-    
-    x2 = np.array([xi**2 for xi in x])
-    x2_mean = x2.mean()
-    
-    chi2_rv = chi2(df = 2 * n)
-    
-    left = chi2_rv.ppf(1 - alpha / 2)
-    right = chi2_rv.ppf(alpha / 2)
-    
-    return np.sqrt(n * x2_mean / (left * 0.22)), np.sqrt(n * x2_mean / (right * 0.22))
+    sum_x2 = sum(x**2)
+    q_l = chi2(2*n).ppf(q=alpha/2)
+    q_r = chi2(2*n).ppf(q=1-alpha/2)
+    return np.sqrt(sum_x2 / q_r / 0.22), np.sqrt(sum_x2 / q_l / 0.22)
